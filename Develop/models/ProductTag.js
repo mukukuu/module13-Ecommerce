a4fe165100fa38e7a61a995efa7aba2280ee6cf1
mukukuu <qinii@me.com>
2022-02-05 22:39:29 -0700

    added associations

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+   id: {
+     type: DataTypes.INTEGER,
+     allowNull: false,
+     primaryKey: true,
+     auto_increment: true
+   },
+   //association with product
+   product_id: {
+     type: DataTypes.INTEGER,
+     references: {
+       model: 'product',
+       key: 'id'
+     }
+   },
+   tag_id: {
+     type: DataTypes.INTEGER,
+     references: {
+       model: 'tag',
+       key: 'id'
+     }
+   }
   },
   {
     sequelize,
